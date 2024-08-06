24d52e4fc1ce76d68d9f652cdaecec4561803764
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-08-06 13:14:05 +0100

    delete folder

--- demo/otp_rest_auth/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class OtpRestAuthConfig(AppConfig):
-    name = "otp_rest_auth"
-    default_auto_field = "django.db.models.BigAutoField"
-
-    def ready(self):
-        import otp_rest_auth.signals
