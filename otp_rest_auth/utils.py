045f155de5bf31f8b565bccef34015bf2a2822b5
Chigozirim Igweamaka <cgzirim@gmail.com>
2025-05-01 03:54:15 +0100

    Enhance user retrieval functions to include additional unique constraint fields for phone, email, and username

--- otp_rest_auth/utils.py
+++ otp_rest_auth/utils.py
@@ -46,38 +46,88 @@ def user_phone(user, *args, commit=False):
     return user_field(user, app_settings.USER_MODEL_PHONE_FIELD, *args, commit=commit)
 
 
-def get_user_by_phone(phone):
+def get_user_by_phone(phone: str, extra: dict):
+    """
+    Get user by phone number and other unique constriant fields.
+    :param phone: Phone number
+    :param extra: Dict containing unique constraint fields, if any.
+    :return: User object if found, else None
+    """
     if not app_settings.UNIQUE_EMAIL:
         return None
 
     User = get_user_model()
+
+    filter_fields = {}
+    const_fields = app_settings.get_user_unique_constraint_fields()
+    if "phone" in const_fields:
+        [
+            filter_fields.update({field: extra[field]})
+            for field in const_fields
+            if field != "phone" and extra.get(field)
+        ]
+
     phone_field = app_settings.USER_MODEL_PHONE_FIELD
-    res = User.objects.filter(**{phone_field: phone})
+    res = User.objects.filter(**{phone_field: phone, **filter_fields})
 
     return res.first()
 
 
-def get_user_by_email(email):
+def get_user_by_email(email: str, extra: dict):
+    """
+    Get user by email and other unique constriant fields.
+    :param email: Email
+    :param extra: Dict containing unique constraint fields, if any.
+    :return: User object or None
+    """
     if not app_settings.UNIQUE_EMAIL:
         return None
 
     User = get_user_model()
+
+    filter_fields = {}
+    const_fields = app_settings.get_user_unique_constraint_fields()
+    if "email" in const_fields:
+        [
+            filter_fields.update({field: extra[field]})
+            for field in const_fields
+            if field != "email" and extra.get(field)
+        ]
+
     email_field = app_settings.USER_MODEL_EMAIL_FIELD
-    res = User.objects.filter(**{email_field: email})
+    res = User.objects.filter(**{email_field: email, **filter_fields})
 
     return res.first()
 
 
-def get_user_by_username(username):
+def get_user_by_username(username: str, extra: dict):
+    """
+    Get user by username and other unique constriant fields.
+    :param username: Username
+    :param extra: Dict containing unique constraint fields, if any.
+    :return: User object or None
+    """
     if not app_settings.UNIQUE_EMAIL:
         return None
 
     User = get_user_model()
+
+    filter_fields = {}
+    const_fields = app_settings.get_user_unique_constraint_fields()
+    if "username" in const_fields:
+        [
+            filter_fields.update({field: extra[field]})
+            for field in const_fields
+            if field.lower() != "username" and extra.get(field)
+        ]
+
     username_field = app_settings.USER_MODEL_USERNAME_FIELD
     if app_settings.PRESERVE_USERNAME_CASING:
-        res = User.objects.filter(**{username_field: username})
+        res = User.objects.filter(**{username_field: username, **filter_fields})
     else:
-        res = User.objects.filter(**{username_field + "__iexact": username.lower()})
+        res = User.objects.filter(
+            **{username_field + "__iexact": username.lower(), **filter_fields}
+        )
 
     return res.first()
 
@@ -99,12 +149,12 @@ def get_auth_method_field(auth_method):
 
     return auth_methods.get(auth_method)
 
-        
+
 def get_throttle_scope(throttle_key):
     rest_config = getattr(settings, "REST_FRAMEWORK", None)
     if rest_config:
         throttle_rates = rest_config.get("DEFAULT_THROTTLE_RATES", {})
         scope = throttle_rates.get(throttle_key, None)
         return throttle_key if scope else ""
-    
+
     return ""
